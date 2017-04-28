85d0097e5826a545d12d75bdc9881f3ea8734b9f
tomasechague <tomiechague@gmail.com>
2017-04-28 10:16:01 -0300

    se empezo a detectar localidad, provincia,ect

--- pruebaApiLocation.php
+++ pruebaApiLocation.php
@@ -1,14 +1,14 @@
 <?php
 
-$direccion = 'PEDRO ECHAGUE 648 9 D';
-$localidad = 'ADROGUE';
+$direccion = '138 1512';
+$localidad = 'LA PLAT';
 $pais = 'Argentina';
-$direccion .= ' '.$localidad.' '.$pais;
+$direccion .= ' ' . $localidad . ' ' . $pais;
 
 $direccion = str_replace('calle', '', $direccion);
 $direccion = str_replace('CALLE', '', $direccion);
 $direccion = str_replace(' ', '%20', $direccion);
-echo $direccion.'<br/>';
+//echo $direccion . '<br/>';
 
 
 /*
@@ -24,16 +24,25 @@ echo $direccion.'<br/>';
 
 
 
-$json = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=Calle%20'.$direccion);
+$json = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=Calle%20' . $direccion);
 $obj = json_decode($json);
-
-if(isset($obj->results[0])){
-//    print_r($obj->results[5]->formatted_address);
-    print_r($obj->results);
-}else{
+//print_r($obj->results[0]->address_components[0]->long_name);
+//die();
+if (isset($obj->results[0])) {
+    
+        print_r($obj->results);
+} else {
     echo "no se encontraron resultados";
 }
 
 //foreach($obj as $result){
 //echo ($result[0]->formatted_address);
 //}
+
+function stdArrayAsArray($objectStd) {
+    foreach ($sa as $key => $value) {
+        $stdArray[$key] = (array) $value;
+    }
+    /*     * * show the results ** */
+    print_r($stdArray);
+}
